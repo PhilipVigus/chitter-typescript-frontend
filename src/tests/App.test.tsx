42ef16091fb5ef93b4946969329239fefc56c158
Phil Vigus <philipvigus@yahoo.co.uk>
2020-07-01 16:04:59 +0100

    Create test folder

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App from "./App";
+import App from "../App";
 
 test("renders Chitter static text", () => {
   const { getByText } = render(<App />);
