eea51bb9cd31cc0baff1342facda48aaa84469b4
Phil Vigus <philipvigus@yahoo.co.uk>
2020-07-02 15:03:43 +0100

    Add axios mocking to retrieve Peeps List

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import App from "../App";
 
-test("renders Chitter static text", () => {
-  const { getByText } = render(<App />);
-  const text = getByText(/Chitter/i);
-  expect(text).toBeInTheDocument();
+test("renders Chitter static text", async () => {
+  const mock = new MockAdapter(axios);
+  mock.onGet("https://localhost:5000/peeps").reply(200, { peeps: [] });
+  const { findByText } = render(<App />);
+  expect(await findByText(/Chitter/)).toBeInTheDocument();
 });
--- src/tests/PeepsList.test.tsx
+++ src/tests/PeepsList.test.tsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import PeepsList from "../components/PeepsList";
 
-test("renders static text", () => {
-  const { getByText } = render(<PeepsList />);
-  expect(getByText(/Peeps List/)).toBeInTheDocument();
-});
+describe("PeepsList", () => {
+  const mock = new MockAdapter(axios);
 
-test("renders list of peeps", () => {
-  const { getByText } = render(<PeepsList />);
-  expect(getByText(/This is a peep/)).toBeInTheDocument();
-  expect(getByText(/This is another peep/)).toBeInTheDocument();
+  beforeAll(() => {
+    mock
+      .onGet("https://localhost:5000/peeps")
+      .reply(200, { peeps: ["Peep 1", "Peep 2"] });
+  });
+
+  afterAll(() => {
+    mock.restore();
+  });
+
+  it("renders static text", async () => {
+    const { findByText } = render(<PeepsList />);
+
+    expect(await findByText(/Peeps List/)).toBeInTheDocument();
+  });
+
+  it("renders list of peeps", async () => {
+    const { findByText } = render(<PeepsList />);
+
+    expect(await findByText(/Peep 1/)).toBeInTheDocument();
+    expect(await findByText(/Peep 2/)).toBeInTheDocument();
+  });
 });
