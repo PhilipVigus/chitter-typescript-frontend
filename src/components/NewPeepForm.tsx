a7b49929443ce3c85142047ad447360ff5c1dcc0
Phil Vigus <philipvigus@yahoo.co.uk>
2020-07-03 09:37:02 +0100

    Add submit button to NewPeepForm
    The button is currently tested by mocking the Axios put method and
    checking that it's called with the appropriate data.

--- src/components/NewPeepForm.tsx
+++ src/components/NewPeepForm.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const NewPeepForm: React.FC = () => {
+  const [text, setText] = useState<string>("");
+
+  const handlePeepSubmit = (evt) => {
+    evt.preventDefault();
+    const data = { text };
+
+    const sendNewPeep = async () => {
+      axios
+        .post("https://localhost:5000/peeps", data)
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error));
+    };
+
+    sendNewPeep();
+  };
   return (
     <div>
       <div>New Peep</div>
-      <textarea />
-      <input type="submit" value="Submit" />
+      <textarea value={text} onChange={(e) => setText(e.target.value)} />
+      <input type="button" value="Submit" onClick={handlePeepSubmit} />
     </div>
   );
 };
