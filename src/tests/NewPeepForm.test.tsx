72c9c279028cb675dbdab94ba218c83788baf357
Phil Vigus <philipvigus@yahoo.co.uk>
2020-07-03 10:03:47 +0100

    Delete unneeded tests

--- src/tests/NewPeepForm.test.tsx
+++ src/tests/NewPeepForm.test.tsx
@@ -21,20 +21,6 @@ describe("NewPeepForm", () => {
     expect(await screen.findByText(/New Peep/)).toBeInTheDocument();
   });
 
-  it("renders a text area you can type in", () => {
-    render(<NewPeepForm />);
-    const textArea = screen.getByRole("textbox") as HTMLInputElement;
-    fireEvent.change(textArea, { target: { value: "Some text" } });
-
-    expect(textArea.value).toBe("Some text");
-  });
-
-  it("renders a submit button for the new peep", () => {
-    render(<NewPeepForm />);
-
-    expect(screen.getByRole("button", { name: "Submit" })).toBeInTheDocument();
-  });
-
   it("posts the peep when you click submit", async () => {
     render(<NewPeepForm />);
     const textArea = screen.getByRole("textbox") as HTMLInputElement;
