ec11c1fdb8ba7c9a881a4af5f637c4043f64bd8a
Phil Vigus <philipvigus@yahoo.co.uk>
2020-07-03 09:44:39 +0100

    Add NewPeepForm to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PeepsList from "./components/PeepsList";
+import NewPeepForm from "./components/NewPeepForm";
 import "./App.css";
 
 function App(): JSX.Element {
   return (
     <div>
       <div>Chitter</div>
+      <NewPeepForm />
       <PeepsList />
     </div>
   );
