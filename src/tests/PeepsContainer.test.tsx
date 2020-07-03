0eb894c7362ff12a7eb9cfffd997ae3c06742b86
Phil Vigus <philipvigus@yahoo.co.uk>
2020-07-03 15:52:43 +0100

    Update PeepsContainer tests
    The tests now include a case that checks to make sure a new peep
    is shown on screen after it has been created.

--- src/tests/PeepsContainer.test.tsx
+++ src/tests/PeepsContainer.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 import PeepsContainer from "../components/PeepsContainer";
@@ -28,5 +28,25 @@ describe("PeepsContainer", () => {
 
     expect(await screen.findByText(/Peep 1/)).toBeInTheDocument();
     expect(await screen.findByText(/Peep 2/)).toBeInTheDocument();
+  });
+
+  it("renders a new peep when it has been created", async () => {
+    mock
+      .onGet("https://localhost:5000/peeps")
+      .reply(200, { peeps: ["Peep 1", "Peep 2"] })
+      .onGet("https://localhost:5000/peeps")
+      .reply(200, { peeps: ["Peep 1", "Peep 2", "Some text"] });
+
+    mock.onPost("https://localhost:5000/peeps").reply(200);
+
+    render(<PeepsContainer />);
+
+    const textArea = (await screen.findByRole("textbox")) as HTMLInputElement;
+    fireEvent.change(textArea, { target: { value: "Some text" } });
+
+    const submitButton = await screen.findByRole("button", { name: "Submit" });
+    fireEvent.click(submitButton);
+
+    expect(await screen.findByText(/Some text/)).toBeInTheDocument();
   });
 });
