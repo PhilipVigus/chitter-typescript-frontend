38020e5dee550815dab71e98695b409a3246b9ea
Phil Vigus <philipvigus@yahoo.co.uk>
2020-07-06 11:55:46 +0100

    Sort peep list by reverse creation date

--- src/components/PeepsList.tsx
+++ src/components/PeepsList.tsx
@@ -9,15 +9,19 @@ const PeepsList: React.FC<PeepsListProps> = ({ peeps }: PeepsListProps) => {
   return (
     <div>
       <div>Peeps List</div>
-      {peeps.map((peep) => {
-        return (
-          <Peep
-            key={peep.text}
-            text={peep.text}
-            timeCreated={peep.timeCreated}
-          />
-        );
-      })}
+      {peeps
+        .sort((a: PeepProps, b: PeepProps) => {
+          return a.timeCreated - b.timeCreated;
+        })
+        .map((peep) => {
+          return (
+            <Peep
+              key={peep.text}
+              text={peep.text}
+              timeCreated={peep.timeCreated}
+            />
+          );
+        })}
     </div>
   );
 };
