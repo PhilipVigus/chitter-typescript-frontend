f1cde189546ea6a9952a204dce9a4365499ce980
Phil Vigus <philipvigus@yahoo.co.uk>
2020-07-06 11:42:29 +0100

    Fix a couple of minor ESLint errors

--- src/tests/PeepsContainer.test.tsx
+++ src/tests/PeepsContainer.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { act, fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 import PeepsContainer from "../components/PeepsContainer";
