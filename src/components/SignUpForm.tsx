c526511b89f8b48086a3626d5c705272fac89464
Phil Vigus <philipvigus@yahoo.co.uk>
2020-07-16 10:25:06 +0100

    Post signup info using axios

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const SignUpForm: React.FC = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSignupSubmit = (
+    evt: React.MouseEvent<HTMLInputElement, MouseEvent>
+  ) => {
+    evt.preventDefault();
+    const data = { username, password };
+
+    const sendSignup = async () => {
+      axios
+        .post("https://localhost:5000/signup", data)
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error));
+    };
+
+    sendSignup();
+  };
+
   return (
     <div>
       <div>
         <label htmlFor="username">
           Username
-          <input type="text" id="username" />
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </label>
       </div>
       <div>
         <label htmlFor="password">
-          <input type="password" id="password" />
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           Password
         </label>
+      </div>
+      <div>
+        <button type="submit" onClick={handleSignupSubmit}>
+          Submit
+        </button>
       </div>
     </div>
   );
--- src/tests/SignUpForm.test.tsx
+++ src/tests/SignUpForm.test.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import SignUpForm from "../components/SignUpForm";
 
 describe("SignupForm", () => {
+  const mock = new MockAdapter(axios);
+
   it("renders the username textbox", () => {
     render(<SignUpForm />);
 
@@ -13,5 +17,28 @@ describe("SignupForm", () => {
     render(<SignUpForm />);
 
     expect(screen.getByLabelText(/Password/)).toBeInTheDocument();
+  });
+
+  it("posts the information when you click signup", async () => {
+    mock.onPost("https://localhost:5000/signup").reply(200);
+    render(<SignUpForm />);
+
+    const usernameField = screen.getByRole("textbox", {
+      name: "Username"
+    }) as HTMLInputElement;
+
+    fireEvent.change(usernameField, { target: { value: "Bob" } });
+    const passwordField = screen.getByLabelText(/Password/) as HTMLInputElement;
+
+    fireEvent.change(passwordField, { target: { value: "1234" } });
+    fireEvent.click(screen.getByRole("button", { name: "Submit" }));
+
+    await waitFor(() => {
+      expect(mock.history.post[0].data).toBe(
+        JSON.stringify({ username: "Bob", password: "1234" })
+      );
+    });
+
+    mock.restore();
   });
 });
