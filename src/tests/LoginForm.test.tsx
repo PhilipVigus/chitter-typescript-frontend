60952e95aa8d80c7ed4ecd841f9b272ef41f3433
PhilipVigus <philipvigus@yahoo.co.uk>
2020-07-22 15:55:13 +0100

    Add Login component and link into the app

--- src/tests/LoginForm.test.tsx
+++ src/tests/LoginForm.test.tsx
@@ -1,39 +1,34 @@
 import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
-import { MemoryRouter as Router } from "react-router-dom";
+import LoginForm from "../components/LoginForm";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
-import MainContainer from "../components/MainContainer";
 
-describe("MainContainer", () => {
+describe("LoginForm", () => {
   const mock = new MockAdapter(axios);
 
   afterAll(() => {
     mock.restore();
   });
 
-  it("renders signup form by by default", () => {
-    render(
-      <Router>
-        <MainContainer />
-      </Router>
-    );
+  it("renders the username textbox", () => {
+    render(<LoginForm />);
 
-    expect(
-      screen.getByRole("heading", { name: "Sign up" })
-    ).toBeInTheDocument();
+    expect(screen.getByRole("heading", { name: "Log in" })).toBeInTheDocument();
   });
 
-  it("renders renders login when you successfully sign up", async () => {
+  it("renders the text boxes", () => {
+    render(<LoginForm />);
+    expect(screen.getByLabelText(/Username/)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Password/)).toBeInTheDocument();
+  });
+
+  it("renders a message when you successfully log in", async () => {
     mock
-      .onPost("http://localhost:5000/users")
+      .onPost("http://localhost:5000/sessions")
       .reply(200, { id: 1, username: "bob" });
 
-    render(
-      <Router>
-        <MainContainer />
-      </Router>
-    );
+    render(<LoginForm />);
 
     const usernameField = screen.getByRole("textbox", {
       name: "Username"
@@ -45,8 +40,6 @@ describe("MainContainer", () => {
     fireEvent.change(passwordField, { target: { value: "1234" } });
     fireEvent.click(screen.getByRole("button", { name: "Submit" }));
 
-    expect(
-      await screen.findByRole("heading", { name: "Log in" })
-    ).toBeInTheDocument();
+    expect(await screen.findByText(/1 - bob/)).toBeInTheDocument();
   });
 });
