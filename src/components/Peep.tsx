c366e84821874a60905ae9ba5159631ee28906e7
PhilipVigus <philipvigus@yahoo.co.uk>
2020-07-27 15:54:36 +0100

    Display peep details

--- src/components/Peep.tsx
+++ src/components/Peep.tsx
@@ -1,12 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+
+type IPeep = {
+  username: string;
+  text: string;
+  timeCreated: string;
+};
 
 const Peep: React.FC = () => {
   const { id } = useParams();
+  const [peep, setPeep] = useState<IPeep>();
+
+  useEffect(() => {
+    const { CancelToken } = axios;
+    const source = CancelToken.source();
+
+    const fetchPeep = async () => {
+      axios
+        .get(`http://localhost:5000/peeps/${id}`, {
+          cancelToken: source.token,
+          withCredentials: true,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json"
+          }
+        })
+        .then((result) => {
+          setPeep(result.data.peep);
+        })
+        .catch((thrown) => {
+          if (axios.isCancel(thrown)) {
+            console.log("Peep get request cancelled");
+          } else {
+            console.log("Error getting peep");
+          }
+        });
+    };
+
+    fetchPeep();
+
+    return () => {
+      source.cancel("Peep get request cancelled");
+    };
+  }, [id]);
+
+  const getTimeCreatedString = () => {
+    if (peep) {
+      const date = new Date(peep?.timeCreated);
+      return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} on ${date.getDate()}-${
+        date.getMonth() + 1
+      }-${date.getFullYear()}`;
+    } else {
+      return "";
+    }
+  };
+
   return (
     <div>
       <h2>Individual peep</h2>
-      {id}
+      {`${peep?.username} - ${peep?.text} - ${getTimeCreatedString()}`}
     </div>
   );
 };
--- src/tests/Peep.test.tsx
+++ src/tests/Peep.test.tsx
@@ -1,16 +1,48 @@
 import React from "react";
-import { MemoryRouter as Router } from "react-router-dom";
+import { MemoryRouter as Router, Route } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import Peep from "../components/Peep";
 
 describe("Peep", () => {
-  it("renders the text", () => {
+  const mock = new MockAdapter(axios);
+
+  beforeAll(() => {
+    const date = new Date(2020, 5, 3, 11, 5, 23);
+
+    mock.onGet("http://localhost:5000/peeps/1").reply(200, {
+      peep: { username: "bob", text: "Peep 1", timeCreated: date }
+    });
+  });
+
+  afterAll(() => {
+    mock.restore();
+  });
+
+  it("renders the title", async () => {
     render(
-      <Router>
-        <Peep />
+      <Router initialEntries={["/peeps/1"]}>
+        <Route path="/peeps/:id">
+          <Peep />
+        </Route>
       </Router>
     );
 
-    expect(screen.getByText(/Individual peep/)).toBeInTheDocument();
+    expect(await screen.findByText(/Individual peep/)).toBeInTheDocument();
+  });
+
+  it("renders the peep details", async () => {
+    render(
+      <Router initialEntries={["/peeps/1"]}>
+        <Route path="/peeps/:id">
+          <Peep />
+        </Route>
+      </Router>
+    );
+
+    expect(await screen.findByText(/bob/)).toBeInTheDocument();
+    expect(await screen.findByText(/Peep 1/)).toBeInTheDocument();
+    expect(await screen.findByText(/11:5:23 on 3-6-2020/)).toBeInTheDocument();
   });
 });
