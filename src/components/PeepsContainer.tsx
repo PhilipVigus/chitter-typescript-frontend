177c35734df8ab4d9e6d70865a2840f02fd1aed3
PhilipVigus <philipvigus@yahoo.co.uk>
2020-07-31 12:13:51 +0100

    Remove username from PeepsContainer render

--- src/components/PeepsContainer.tsx
+++ src/components/PeepsContainer.tsx
@@ -1,15 +1,11 @@
-import React, { useContext } from "react";
+import React from "react";
 import NewPeepForm from "./NewPeepForm";
 import PeepsList from "./PeepsList";
-import { MainContext } from "../contexts/MainContext";
 import "./PeepsContainer.css";
 
 const PeepsContainer: React.FC = () => {
-  const [userState] = useContext(MainContext);
-
   return (
     <div className="peeps-container">
-      {userState.name}
       <NewPeepForm />
       <PeepsList />
     </div>
